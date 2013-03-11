b1e5ee3e9f6b4395d9e18f1b4459d503c6cc19af
unknown <esimorre@PO8916.IDSI0.si.c-s.fr>
2013-03-11 10:50:37 +0100

    rights: WIP

--- liteflow/rights/admin.py
+++ liteflow/rights/admin.py
@@ -2,15 +2,37 @@
 from django.contrib import admin
 from models import *
 
+# actions
+def action_copy(modeladmin, request, queryset):
+    pass
+action_copy.short_description = "Copy rights (NYI)"
+
+def action_paste(modeladmin, request, queryset):
+    pass
+action_paste.short_description = "Paste rights (NYI)"
+
 
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    actions = [action_copy, action_paste]
+    
 admin.site.register(Organization, OrganizationAdmin)
 
 class RightAdmin(admin.ModelAdmin):
-    list_display = ('content_type', 'organization', 'group', 'operation')
-    list_filter = ('organization', 'group', 'operation', 'content_type')
+    list_display = ('organization', 'content_type', 'group', 'operation', 'state')
+    list_filter = ('organization', 'operation', 'content_type', 'group')
 admin.site.register(Right, RightAdmin)
+
+
+class RightModelAdmin(admin.ModelAdmin):
+    right_fields = ('state', 'organization')
+    def queryset(self, request):
+        qs = super(RightModelAdmin, self).queryset(request)
+        if 'organization' in self.right_fields:
+            qs = qs.filter(organization__in=Right.objects.visible_organizations_names(request.user))
+        if 'state' in self.right_fields:
+            qs = qs.filter(state__in=Right.objects.visible_states(request.user, self.model))
+        return qs
 
 
 from django.contrib.auth.models import Permission
--- liteflow/rights/models.py
+++ liteflow/rights/models.py
@@ -11,20 +11,60 @@ class Organization(models.Model):
 
 OPERATION_CHOICES = (
 (u'Any', u'Any'),
-(u'Creation', u'Creation'),
+(u'Read', u'Read'),
+(u'Update', u'Update'),
+(u'Create', u'Create'),
 (u'Manage', u'Manage'),
 (u'Oper1', u'Oper1'),
 )
 
 STATE_CHOICES = (
+(u'Any', u'Any'),
 (u'created', u'created'),
 (u'valid', u'valid'),
 (u'released', u'released'),
 (u'obsolete', u'obsolete'),
 )
+
+class RightManager(models.Manager):
+    # cache[username] = {'orgas': list_orga_names, }
+    cache = {}
+    def visible_organizations(self, user):
+        return self.get_rights(user).values_list('organization')
+    
+    def visible_organizations_names(self, user):
+        if not self.cache.has_key(user.username):
+            self.cache[user.username] = {}
+        if not self.cache[user.username].has_key('orgas'):
+            self.cache[user.username]['orgas'] = [o.name for o in self.visible_organizations(user)]
+        return self.cache[user.username]['orgas']
+    
+    def visible_states_names(self, user, model):
+        model_name = model._meta.object_name
+        if not self.cache.has_key(user.username):
+            self.cache[user.username] = {}
+        if not self.cache[user.username].has_key('states'):
+            self.cache[user.username]['states'] = {}
+        if not self.cache[user.username]['states'].has_key(model_name):
+            self.cache[user.username]['states'][model_name] = [r.state for r in self.get_rights(user, model)]
+        return self.cache[user.username]['states'][model_name]
+    
+    def reset_cache(self, user=None):
+        if not user: self.cache = {}
+        else:
+            self.cache[user.username] = {}
+    
+    def get_rights(self, user, model=None):
+        qs = self.filter(group__in=user.groups)
+        if model:
+            return qs.filter(content_type=ContentType.objects.get_for_model(model))
+
 class Right(models.Model):
     content_type = models.ForeignKey(ContentType, null=True, blank=True)
     group = models.ForeignKey(Group, null=True, blank=True)
     organization = models.ForeignKey(Organization, null=True, blank=True)
     operation = models.CharField(max_length=50, choices=OPERATION_CHOICES)
     state = models.CharField(max_length=30, null=True, blank=True, choices=STATE_CHOICES)
+    
+    objects = RightManager()
+    
